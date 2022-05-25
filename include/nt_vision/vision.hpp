cbc3894a86744e4af326292d98d4cf472a8d098c
Filipe Verri <filipeverri@gmail.com>
2022-05-25 10:51:00 -0300

    First commit.

---
+++ include/nt_vision/vision.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace nt
+{
+namespace vision
+{
+void do_something();
+} // namespace vision
+} // namespace nt
---
+++ src/vision.cpp
@@ -0,0 +1,13 @@
+#include <nt_io/io.hpp>
+
+#include "nt_vision/vision.hpp"
+
+namespace nt
+{
+namespace vision
+{
+void do_something() {
+  ::nt::io::print_something();
+}
+} // namespace vision
+} // namespace nt
